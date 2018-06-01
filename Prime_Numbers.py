167984f4ad78d7bfe626667ac88965cb20ed6769
mosamp <36801202+mosamp@users.noreply.github.com>
2018-06-01 15:08:15 -0400

    Prime Numbers Logic Implementation

---
+++ Prime_Numbers.py
@@ -0,0 +1,19 @@
+# Python program to check if the input number is Prime or Not
+
+
+num = int(input("Enter a number: "))
+
+
+if num > 1:
+
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+
+           break
+   else:
+       print(num,"is a prime number")
+
+
+else:
+   print(num,"is not a prime number")
